d4f8fb437694bfeaa195938c86af7dbf7a5d3238
David <ley.chang@hotmail.com>
2024-03-27 21:17:34 -0600

    .

--- index.js
+++ index.js
@@ -20,48 +20,14 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: { type: String, required: true },
-});
-
-const exerciseSchema = new Schema({
-  _id: { type: String, required: true },
-  username: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  description: { type: String, required: true },
-});
-
-const logSchema = new Schema({
-  _id: { type: String, required: true },
-  count: { type: Number, required: true },
-  username: { type: String, required: true },
-  log: [
-    {
-      description: { type: String, required: true },
-      duration: { type: Number, required: true },
-      date: { type: String, required: true },
-    },
-  ],
+  description: { type: String },
+  duration: { type: Number },
+  date: { type: String },
+  count: { type: Number },
+  log: { type: [Object] },
 });
 
 let User = mongoose.model("User", userSchema);
-let Exercise = mongoose.model("Exercise", exerciseSchema);
-let Log = mongoose.model("Log", logSchema);
-
-/* Example mongoose saving */
-/* 
-const createAndSavePerson = function (done) {
-  const janeFonda = new Person({
-    name: "Jane Fonda",
-    age: 84,
-    favoriteFoods: ["eggs", "fish", "fresh fruit"],
-  });
-
-  janeFonda.save(function (err, data) {
-    if (err) return console.error(err);
-    done(null, data);
-  });
-};
-*/
 
 app.use(cors());
 app.use(express.static("public"));
@@ -72,10 +38,14 @@ app.get("/", (req, res) => {
 app.post("/api/users", (req, res) => {
   const doc = new User({
     username: req.body.username,
+    count: 0,
+    log: [],
   });
   doc.save();
   const data = doc.toObject();
-  return res.json(data);
+  const { _id, username } = data;
+  const filtered = { _id, username };
+  return res.json(filtered);
 });
 
 app.get("/api/users", (req, res) => {
@@ -85,42 +55,44 @@ app.get("/api/users", (req, res) => {
       res.status(500).json({ error: "Error al buscar usuarios" });
       return;
     }
-
     res.json(documentos);
-  });
+  }).select("_id username");
 });
 
-app.post("/api/users/:_id/exercises", async (req, res) => {
-  const valida_fecha = new Date(req.body.date);
-  const _id = req.body[":_id"];
-  let date;
+app.post("/api/users/:_id/exercises", (req, res) => {
+  try {
+    const valida_fecha = new Date(req.body.date);
+    const _id = req.body[":_id"];
+    const description = req.body.description;
+    const duration = parseFloat(req.body.duration);
+    let date;
 
-  if (!isNaN(valida_fecha) && valida_fecha instanceof Date) {
-    date =
-      req.body.date.trim() !== ""
-        ? new Date(req.body.date).toDateString()
-        : new Date().toDateString();
-  } else {
-    date = new Date().toDateString();
+    if (!isNaN(valida_fecha) && valida_fecha instanceof Date) {
+      date =
+        req.body.date.trim() !== ""
+          ? new Date(req.body.date).toDateString()
+          : new Date().toDateString();
+    } else {
+      date = new Date().toDateString();
+    }
+    const user = User.findById(_id, (err, user) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      user.description = description;
+      user.duration = duration;
+      user.date = date;
+      user.save();
+    });
+    if (!user) {
+      res.status(404).json({ error: "Usuario no encontrado" });
+      return;
+    }
+    res.json(user);
+  } catch (error) {
+    return res.json(error);
   }
-  const exercise = {
-    _id,
-    description: req.body.description,
-    duration: parseFloat(req.body.duration),
-    date,
-  };
-  const user = await User.findById(_id);
-  if (!user) {
-    res.status(404).json({ error: "Usuario no encontrado" });
-    return;
-  }
-
-  exercise["username"] = user.username;
-
-  const doc = new Exercise(exercise);
-  await doc.save();
-  const data = doc.toObject();
-  res.json(data);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
